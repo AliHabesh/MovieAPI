b1f509e99bc4d072c9eb2a3774d880d573b7fb8c
AliHabesh <ali.habesh0068@stud.hkr.se>
2023-03-02 09:01:42 +0100

    finished the franchisse controller class and changed mapping structor for Franchise

--- src/main/java/com/example/MovieAPI/service/FranchiseService.java
+++ src/main/java/com/example/MovieAPI/service/FranchiseService.java
@@ -1,16 +1,13 @@
 package com.example.MovieAPI.service;
 
-import com.example.MovieAPI.dto.CharacterDTO;
-import com.example.MovieAPI.dto.FranchiseDTO;
-import com.example.MovieAPI.dto.MovieDTO;
-import com.example.MovieAPI.mapper.CharacterDtoMapper;
-import com.example.MovieAPI.mapper.FranchiseDtoMapper;
-import com.example.MovieAPI.model.Character;
-import com.example.MovieAPI.model.Franchise;
-import com.example.MovieAPI.repositories.CharacterRepository;
-import com.example.MovieAPI.repositories.FranchiseRepository;
-import org.springframework.stereotype.Service;
 
+import com.example.MovieAPI.dto.FranchiseDTO;
+import com.example.MovieAPI.mapper.FranchiseDtoMapperImplementation;
+import com.example.MovieAPI.model.Franchise;
+import com.example.MovieAPI.model.Movie;
+import com.example.MovieAPI.repositories.FranchiseRepository;
+import com.example.MovieAPI.repositories.MovieRepository;
+import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -18,25 +15,28 @@ import java.util.Optional;
 public class FranchiseService {
 
 
-    private FranchiseRepository franchiseRepository;
 
-    public FranchiseService(FranchiseRepository franchiseRepository) {
+    private FranchiseDtoMapperImplementation franchiseDtoMapperImplementation;
+    private FranchiseRepository franchiseRepository;
+    private MovieRepository movieRepository;
+
+    public FranchiseService(FranchiseDtoMapperImplementation franchiseDtoMapperImplementation, FranchiseRepository franchiseRepository, MovieRepository movieRepository) {
+        this.franchiseDtoMapperImplementation = franchiseDtoMapperImplementation;
         this.franchiseRepository = franchiseRepository;
+        this.movieRepository = movieRepository;
     }
 
-
-
-        public FranchiseDTO saveFranchise(FranchiseDTO franchiseDTO){
+    public FranchiseDTO saveFranchise(FranchiseDTO franchiseDTO){
             if (franchiseDTO == null) return null;
-            Franchise franchise = franchiseRepository.save(FranchiseDtoMapper.INSTANCE.franchiseDtoToFranchise(franchiseDTO));
-            return franchise != null ? FranchiseDtoMapper.INSTANCE.franchiseToFranchiseDto(franchise):null;
+            Franchise franchise = franchiseRepository.save(franchiseDtoMapperImplementation.franchiseDtoToFranchise(franchiseDTO));
+            return franchise != null ? franchiseDtoMapperImplementation.franchiseToFranchiseDto(franchise):null;
         }
 
         public FranchiseDTO getFranchiseByName(String name){
             if (name == null || name.isEmpty()) return null;
 
             Franchise franchise = franchiseRepository.findByName(name);
-            return franchise != null ? FranchiseDtoMapper.INSTANCE.franchiseToFranchiseDto(franchise):null;
+            return franchise != null ? franchiseDtoMapperImplementation.franchiseToFranchiseDto(franchise):null;
         }
 
         public FranchiseDTO getFranchiseById(int id){
@@ -45,13 +45,13 @@ public class FranchiseService {
             Optional<Franchise> franchiseOptional = franchiseRepository.findById(id);
             Franchise franchise = franchiseOptional.get();
 
-            return franchise != null ? FranchiseDtoMapper.INSTANCE.franchiseToFranchiseDto(franchise):null;
+            return franchise != null ? franchiseDtoMapperImplementation.franchiseToFranchiseDto(franchise):null;
         }
 
         public List<FranchiseDTO> getAllFranchises(){
             List<Franchise> franchises = franchiseRepository.findAll();
             List<FranchiseDTO> franchiseDTOList = convertListFranchiseToDto(franchises);
-            return franchiseDTOList != null ? franchiseDTOList: null;
+            return franchiseDTOList != null ? franchiseDTOList : null;
         }
 
         private List<FranchiseDTO> convertListFranchiseToDto(List<Franchise> franchises){
@@ -59,17 +59,36 @@ public class FranchiseService {
                 return null;
 
             ArrayList<FranchiseDTO> franchiseDTOArrayList = new ArrayList<>();
-            franchises.forEach(value -> franchiseDTOArrayList.add(FranchiseDtoMapper.INSTANCE.franchiseToFranchiseDto(value)));
+            franchises.forEach(value -> franchiseDTOArrayList.add(franchiseDtoMapperImplementation.franchiseToFranchiseDto(value)));
             return franchiseDTOArrayList;
         }
 
-        public FranchiseDTO addMovieToFranchise(MovieDTO movieDTO, String characterName){
-            FranchiseDTO franchiseDTO = FranchiseDtoMapper.INSTANCE.franchiseToFranchiseDto(franchiseRepository.findByName(characterName));
-            return null;
+        /*
+        public FranchiseDTO addMovieToFranchise(int franchiseId, int movieId){
+            FranchiseDTO franchiseDTO = franchiseDtoMapperImplementation.franchiseToFranchiseDto(franchiseRepository.findById(franchiseId).get());
+            Movie movie = movieRepository.findById(movieId).get();
+            movie.setFranchise(franchiseDtoMapperImplementation.franchiseDtoToFranchise(franchiseDTO));
+            franchiseDTO.getMovies().add(movie.getMovieId());
+            franchiseRepository.save(franchiseDtoMapperImplementation.franchiseDtoToFranchise(franchiseDTO));
+            movieRepository.save(movie);
+            return franchiseDTO;
         }
+
+         */
 
         public int deleteFranchiseById(Integer id){
             franchiseRepository.deleteById(id);
             return 1;
+        }
+
+        public List<Integer> getAllCharactersInFranchise(int franchiseId){
+            Franchise franchise = franchiseRepository.findById(franchiseId).get();
+            ArrayList<Integer> characterList = new ArrayList<>();
+            franchise.getMovies().forEach(movie -> {
+                movie.getCharacterList().forEach(character -> {
+                    characterList.add(character.getCharacterId());
+                });
+            });
+            return characterList;
         }
 }
